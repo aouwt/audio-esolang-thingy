c98521cc16a30786cb3d1e849779db791d91bc8c
aouwt <74026992+aouwt@users.noreply.github.com>
2022-01-28 10:46:23 -0500

    i think im getting the hang of this

--- audio.cpp
+++ audio.cpp
@@ -1,15 +1,50 @@
 #include <kiss_fftr.h>
+#include <math.h>
 
-#define NFFT 1023
-kiss_fftr_cfg kiss = NULL;
+#define IABS(n)	sqrt (((n).i * (n).i) + ((n).r * (n).r))
+#define F2I(f)	((int) (SRATE / (f)))
 
-void init (void) {
-	kiss = kiss_fftr_alloc (NFFT + 1, 0, NULL, NULL);
+#define SAMPS	((size_t) (audlen / NFFT))
+
+#define NFFT 1024
+#define SRATE 44100
+#define SENSE 1
+kiss_fft_scalar *audbuf = NULL;
+int nfft;
+
+typedef struct {
+	float amp [NFFT];
+} Processed;
+
+// <AMPLITUDE> == Processed [<POSITION>].amp [F2I (<FREQUENCY>)]
+
+void process (Processed aud []) {
+	for (size_t audpos = 0; audpos != audlen; audpos ++) {
+		// Process audio segment
+		
+		kiss_fft_scalar out [NFFT];
+		kiss_ffr_cfg kiss = kiss_fftr_alloc (NFFT, 0, NULL, NULL);
+		kiss_fftr (kiss, audbuf + (audpos * NFFT), &out);
+		
+		for (size_t i = 0; i != NFFT; i ++) {
+			// Process item of OUT (individual cycle, frequencies)
+			aud [audpos].amp [i] = IABS (out [i]);
+		}
+		
+		kiss_fftr_free (kiss);
+	}
 }
 
-void process (kiss_fft_scalar *audbuf) {
-	kiss_fft_scalar freqs [NFFT];
+void interp (Processed aud []) {
+	float amp;
 	
-	kiss_fftr (kiss, audbuf, &freqs);
+	#define DEF(freq, code) \
+		if ((amp = aud [i].amp [F2I (freq)]) >= SENSE) amp;
 	
-	
+	for (size_t i = 0; i != SAMPS; i ++) {
+		DEF (440, {
+			printf ("hi");
+		})
+	}
+	#undef DEF
+}
