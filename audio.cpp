88a05fe2cf4f46b362c616e33c4f61a94a99f052
aouwt <74026992+aouwt@users.noreply.github.com>
2022-01-29 20:54:05 -0500

    .

--- audio.cpp
+++ audio.cpp
@@ -1,4 +1,4 @@
-#include <kiss_fftr.h>
+#include <kissfft/kiss_fftr.h>
 #include <math.h>
 
 #define IABS(n)	sqrt (((n).i * (n).i) + ((n).r * (n).r))
@@ -9,42 +9,49 @@
 #define NFFT 1024
 #define SRATE 44100
 #define SENSE 1
-kiss_fft_scalar *audbuf = NULL;
-int nfft;
+kiss_fft_scalar *audbuf = NULL; size_t audlen = 0;
 
-typedef struct {
-	float amp [NFFT];
-} Processed;
 
-// <AMPLITUDE> == Processed [<POSITION>].amp [F2I (<FREQUENCY>)]
-
-void process (Processed aud []) {
-	for (size_t audpos = 0; audpos != audlen; audpos ++) {
-		// Process audio segment
+void fft (void) {
+	float avglevel = 0;
+	float *taud = new float [NFFT] [audlen]; // AMP = [FREQ] [POS]
+	
+	
+	// STAGE 1:  FFT routine
+	{	kiss_fftr_cfg kiss = kiss_fftr_alloc (NFFT, 0, NULL, NULL);
 		
-		kiss_fft_scalar out [NFFT];
-		kiss_ffr_cfg kiss = kiss_fftr_alloc (NFFT, 0, NULL, NULL);
-		kiss_fftr (kiss, audbuf + (audpos * NFFT), &out);
-		
-		for (size_t i = 0; i != NFFT; i ++) {
-			// Process item of OUT (individual cycle, frequencies)
-			aud [audpos].amp [i] = IABS (out [i]);
+		float totalavg = 0;
+		for (size_t audpos = 0; audpos != audlen; audpos ++) {
+			float segavg = 0;
+			
+			// Process audio segment	
+			kiss_fft_scalar out [NFFT];
+			kiss_fftr (kiss, audbuf + (audpos * NFFT), &out);
+			
+			for (size_t i = 0; i != NFFT; i ++) {
+				// Process item of OUT (individual cycle, frequencies)
+				segavg += taud [i] [audpos] = IABS (out [i]);
+			}
+			
+			totalavg += segavg / NFFT;
 		}
+		avglevel = totalavg / audlen;
 		
 		kiss_fftr_free (kiss);
 	}
+	
+	
+	// STAGE 2: take out noise
+	{
+		for (size_t audpos = 0; audpos != audlen; audpos ++) {
+			for (size_t i = 0; i != NFFT; i ++) {
+				if (taud [i] [audpos] > avglevel) {
+					
+				}
+			}
+		}
+	}
+	
+	delete[] taud;
 }
 
-void interp (Processed aud []) {
-	float amp;
-	
-	#define DEF(freq, code) \
-		if ((amp = aud [i].amp [F2I (freq)]) >= SENSE) amp;
-	
-	for (size_t i = 0; i != SAMPS; i ++) {
-		DEF (440, {
-			printf ("hi");
-		})
-	}
-	#undef DEF
-}
