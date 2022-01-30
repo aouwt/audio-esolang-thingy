a076d5e7d277d75bc7dbc052d0e1cfc495dd7314
aouwt <74026992+aouwt@users.noreply.github.com>
2022-01-29 21:07:27 -0500

    .

--- audio.cpp
+++ audio.cpp
@@ -2,31 +2,42 @@
 #include <math.h>
 
 #define IABS(n)	sqrt (((n).i * (n).i) + ((n).r * (n).r))
-#define F2I(f)	((int) (SRATE / (f)))
+#define F2I(f)	((kiss_fft_scalar) (SRATE / (f)))
 
 #define SAMPS	((size_t) (audlen / NFFT))
 
 #define NFFT 1024
 #define SRATE 44100
 #define SENSE 1
+
+typedef struct {
+	kiss_fft_scalar *freq [NFFT] = { NULL };
+	kiss_fft_scalar dat [NFFT] = { 0 };
+} Program;
+
+Program *prg = NULL; size_t prglen = 0;
+
 kiss_fft_scalar *audbuf = NULL; size_t audlen = 0;
 
 
 void fft (void) {
+	prglen = audlen / NFFT;
+	
+	
 	float avglevel = 0;
-	float *taud = new float [NFFT] [audlen]; // AMP = [FREQ] [POS]
+	float taud [NFFT] [prglen]; // AMP = [FREQ] [POS]
 	
 	
 	// STAGE 1:  FFT routine
 	{	kiss_fftr_cfg kiss = kiss_fftr_alloc (NFFT, 0, NULL, NULL);
 		
-		float totalavg = 0;
-		for (size_t audpos = 0; audpos != audlen; audpos ++) {
+		double totalavg = 0;
+		for (size_t audpos = 0; audpos != prglen; audpos ++) {
 			float segavg = 0;
 			
 			// Process audio segment	
-			kiss_fft_scalar out [NFFT];
-			kiss_fftr (kiss, audbuf + (audpos * NFFT), &out);
+			kiss_fft_cpx out [NFFT];
+			kiss_fftr (kiss, audbuf + (audpos * NFFT), out);
 			
 			for (size_t i = 0; i != NFFT; i ++) {
 				// Process item of OUT (individual cycle, frequencies)
@@ -35,23 +46,22 @@ void fft (void) {
 			
 			totalavg += segavg / NFFT;
 		}
-		avglevel = totalavg / audlen;
+		avglevel = totalavg / prglen;
 		
 		kiss_fftr_free (kiss);
 	}
 	
 	
+	prg = new Program [prglen];
 	// STAGE 2: take out noise
 	{
-		for (size_t audpos = 0; audpos != audlen; audpos ++) {
+		for (size_t prgpos = 0; prgpos != prglen; prgpos ++) {
 			for (size_t i = 0; i != NFFT; i ++) {
-				if (taud [i] [audpos] > avglevel) {
-					
+				if (taud [i] [prgpos] > avglevel) {
+					prg [prgpos].dat [
 				}
 			}
 		}
 	}
-	
-	delete[] taud;
 }
 
