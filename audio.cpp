8acf21274262f3f6e1b6a496d712c2b455b9c74a
aouwt <74026992+aouwt@users.noreply.github.com>
2022-01-31 20:22:01 -0500

    .

--- audio.cpp
+++ audio.cpp
@@ -2,12 +2,16 @@
 #include <errno.h>
 #include <math.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
 
-#define IABS(n)	sqrt (((n).i * (n).i) + ((n).r * (n).r))
+#define IABS(n)	sqrt ((n.i * n.i) + (n.r * n.r))
 #define F2I(f)	((kiss_fft_scalar) (SRATE / (f)))
 
 #define SAMPS	((size_t) (audlen / NFFT))
-#define EFFECTIVE_NFFT ((NFFT * 2) + 1)
+#define EFFECTIVE_NFFT (NFFT * 2)
 
 #define NFFT 1024
 #define SRATE 44100
@@ -24,12 +28,22 @@ Program *prg = NULL; size_t prglen = 0;
 kiss_fft_scalar *audbuf = NULL; size_t audlen = 0;
 
 
+
+size_t get_fd_len (int fd) {
+	size_t b;
+	if (ioctl(fd, FIONREAD, &b)) {
+		perror ("ioctl");
+		_exit (1);
+	}
+	return b;
+}
+
 void fft (void) {
 	prglen = audlen / EFFECTIVE_NFFT;
 	
 	
 	float avglevel = 0;
-	float taud [NFFT] [prglen]; // AMP = [FREQ] [POS]
+	float taud [NFFT + 1] [prglen + 1]; // AMP = [FREQ] [POS]
 	
 	
 	// STAGE 1:  FFT routine
@@ -40,12 +54,12 @@ void fft (void) {
 			float segavg = 0;
 			
 			// Process audio segment	
-			kiss_fft_cpx out [NFFT];
+			kiss_fft_cpx out [NFFT + 1];
 			kiss_fftr (kiss, audbuf + (audpos * EFFECTIVE_NFFT), out);
 			
 			for (size_t i = 0; i != NFFT; i ++) {
 				// Process item of OUT (individual cycle, frequencies)
-				segavg += taud [i] [audpos] = IABS (out [i]);
+				segavg += (taud [i] [audpos] = IABS (out [i]));
 			}
 			
 			totalavg += segavg / NFFT;
@@ -75,6 +89,8 @@ void fft (void) {
 void load_audfile (const char *path) {
 	int fds [2];
 	
+	fprintf (stderr, "Launching ffmpeg to load file...");
+	
 	if (pipe (fds) == -1) {
 		perror ("pipe");
 		_exit (1);
@@ -88,7 +104,7 @@ void load_audfile (const char *path) {
 		close (fds [0]);
 		close (fds [1]);
 	
-		exec ("ffmpeg", "-stats", "-i", path, "-acodec", "pcm_u8", "-ar", "48000", "-ac", "1");
+		execlp ("ffmpeg", "", "-loglevel", "0", "-i", path, "-acodec", "pcm_u8", "-ar", "48000", "-ac", "1", "-f", "u8", "-", NULL);
 		
 		perror ("exec");
 		_exit (1);
@@ -100,23 +116,35 @@ void load_audfile (const char *path) {
 	}
 	
 	close (fds [1]);
+	waitpid (pid, NULL, 0);
 	
+	fprintf (stderr, "Done\n");
+	fprintf (stderr, "Loading file...");
 	// Load into audbuf
 	{	int fd = fds [0];
-		audlen = lseek (fd, 0, SEEK_END);
-		
-		lseek (fd, 0, SEEK_SET);
+		audlen = get_fd_len (fd);
 		
 		audbuf = (kiss_fft_scalar *) malloc (sizeof (kiss_fft_scalar) * (audlen + 1));
+		if (audbuf == NULL) {
+			perror ("malloc");
+			_exit (1);
+		}
+		
 		// cant use one big read () bc we need to cast!!
 		for (size_t i = 0; i != audlen; i ++) {
-			uint8_t b;
-			read (fd, &b, sizeof (b));
+			signed char b;
+			read (fd, &b, 1);
 			
 			audbuf [i] = (kiss_fft_scalar) b;
 		}
+		
+		close (fd);
 	}
+	fprintf (stderr, "Done\n");
+	fprintf (stderr, "%i bytes loaded.\n", audlen);
 }
 
 int main (void) {
+	load_audfile ("a.wav");
+	fft ();
 }
